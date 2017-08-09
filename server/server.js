af5a72704b1f5a6c2095ad582e1e4dcfea40f761
Reggie Cooper <reggie@Reggies-MacBook-Pro.local>
2017-08-09 07:59:28 -0700

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -49,6 +49,22 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    // console.log(todo);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 app.listen(port, () => {
   console.log(`Started on Port ${port}`);
 });
